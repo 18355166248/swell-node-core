554007e0b057ed3ca08e532ff47075e0d6a5d101
jianglang <lang.jiang@ximalaya.com>
2023-10-11 00:16:35 +0800

    feat: init jest

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/src/"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  // setupFilesAfterEnv: ["<rootDir>/scripts/testSetup.js"],
+  collectCoverage: true,
+  collectCoverageFrom: ["!/node_modules/", "!build/", "src/**/*.ts"],
+};
